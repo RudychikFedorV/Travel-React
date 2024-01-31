4a8967f8d1292310927be2b850026ba4153dc0cc
RudychikFedorV <rudychik.fedor@gmail.com>
2024-01-31 20:48:41 +0200

    active

--- src/App.jsx
+++ src/App.jsx
@@ -5,47 +5,70 @@ import { SplashScreen } from "./components/SplashScreen";
 import { useState } from "react";
 import { MenuIcon } from "./components/Icons/MenuIcon";
 import { useLocation } from "react-router-dom";
+import NotFound from "./views/Home/NotFound";
 
 function App() {
-	const location = useLocation();
-	const currentUrl = location.pathname;
+  const location = useLocation();
+  const currentUrl = location.pathname;
 
-	console.log(currentUrl);
-	const [isSpinner, setIsSpinner] = useState(true);
+  console.log(currentUrl);
+  const [isSpinner, setIsSpinner] = useState(true);
 
-	setTimeout(() => {
-		setIsSpinner(false);
-	}, 3000);
-	return (
-		<>
-			{!isSpinner && (
-				<>
-					<Routes>
-						<Route path="/" element={<Home />}></Route>
-						<Route path="/travel" element={<TravelPage />}></Route>
-					</Routes>
-					{currentUrl !== "/travel" && (
-						<header>
-							{/* <Link to="/travel"><MenuIcon type='home'/></Link> */}
-							<NavLink exact activeClassName="is-active" to="/">
-								<MenuIcon type="home" />
-							</NavLink>
-							<NavLink activeClassName="is-active" to="/history">
-								<MenuIcon type="history" />
-							</NavLink>
-							<NavLink activeClassName="is-active" to="/heart">
-								<MenuIcon type="heart" />
-							</NavLink>
-							<NavLink activeClassName="is-active" to="/profile">
-								<MenuIcon type="profile" />
-							</NavLink>
-						</header>
-					)}
-				</>
-			)}
-			{isSpinner && <SplashScreen />}
-		</>
-	);
+  setTimeout(() => {
+    setIsSpinner(false);
+  }, 3000);
+  const [tab, setTab] = useState(1);
+  return (
+    <>
+      {!isSpinner && (
+        <>
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/travel" element={<TravelPage />}></Route>
+			<Route path="*" element={<NotFound />} />
+          </Routes>
+          {currentUrl !== "/travel" && (
+            <header className="home__header">
+              <div
+                onClick={() => setTab(1)}
+                className={`home__header ${tab === 1 ? "is-active" : ""}`}
+              >
+                <NavLink exact activeClassName="is-active" to="/">
+                  <MenuIcon type="home" />
+                </NavLink>
+              </div>
+
+              <div
+                onClick={() => setTab(2)}
+                className={`home__header ${tab === 2 ? "is-active" : ""}`}
+              >
+                <NavLink activeClassName="is-active" to="/history">
+                  <MenuIcon type="history" />
+                </NavLink>
+              </div>
+              <div
+                onClick={() => setTab(3)}
+                className={`home__header ${tab === 3 ? "is-active" : ""}`}
+              >
+                <NavLink activeClassName="is-active" to="/heart">
+                  <MenuIcon type="heart" />
+                </NavLink>
+              </div>
+              <div
+                onClick={() => setTab(4)}
+                className={`home__header ${tab === 4 ? "is-active" : ""}`}
+              >
+                <NavLink activeClassName="is-active" to="/profile">
+                  <MenuIcon type="profile" />
+                </NavLink>
+              </div>
+            </header>
+          )}
+        </>
+      )}
+      {isSpinner && <SplashScreen />}
+    </>
+  );
 }
 
 export default App;
--- src/views/Home/components/HomeHeader.jsx
+++ src/views/Home/components/HomeHeader.jsx
@@ -1,15 +1,13 @@
-
-
 export const HomeHeader = () => {
-	return (
-		<div className="home-header">
-			<div className="home-header__info">
-				<div className="home-header__hi">Hi, David 👋</div>
-				<div className="home-header__text">Explore the world</div>
-			</div>
-			<div className="home-header__img">
-				<img src="./public/user.png" alt="User" />
-			</div>
-		</div>
-	);
+  return (
+    <div className="home-header">
+      <div className="home-header__info">
+        <div className="home-header__hi">Hi, David 👋</div>
+        <div className="home-header__text">Explore the world</div>
+      </div>
+      <div className="home-header__img">
+        <img src="./public/user.png" alt="User" />
+      </div>
+    </div>
+  );
 };
